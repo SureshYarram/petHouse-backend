0917d41eb6483081f0029849cbe2a5a035dc95de
Suresh Yarram <96100384+SureshYarram@users.noreply.github.com>
2022-06-15 11:26:47 +0530

    Update db.js

--- src/config/db.js
+++ src/config/db.js
@@ -4,4 +4,3 @@ module.exports = ()=>{
     return mongoose.connect("mongodb+srv://suresh:suresh_123@cluster0.t9hji.mongodb.net/PetHouseData?retryWrites=true&w=majority")
 }
 
-// mongodb+srv://Prachi:prachi_123@cluster0.o9cs6.mongodb.net/PetHouseData?retryWrites=true&w=majority
